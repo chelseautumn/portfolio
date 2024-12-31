cee1350be3792883d02001164f4da83569f874fe
cverh <cverheyen@wisc.edu>
2024-12-30 20:32:16 -0600

    memoize projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,36 +1,38 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import Card from "../components/Card.jsx";
 import "../styles/Projects.css";
 import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
 import { FidgetSpinner } from "react-loader-spinner";
 
-const Portfolio = () => {
-  return (
-    <div className="portfolio">
-      <img src="/folio.png" alt="portfolio" width="85%"/>
-      <p>A React app showcasing my work, including a dynamic CSS grid layout and interactive designs. Built with Vite and hosted via Github Pages.</p>
-    </div>
-  );
-};
-
-const Bounce = () => {
-  return (
-    <div className="bounce">
-      <video src="/bounce_demo.MOV" autoPlay loop playsInline />
-      <p>An iOS arcade game built with Swift and published to the App Store featuring monetized ads, leaderboard, and achievements.</p>
-    </div>
-  );
-}
-
-const Loader = () => {
-  return (
-    <div className="spinner">
-      <FidgetSpinner height={200} width={440} backgroundColor="var(--highlight-color)" ballColors={["var(--text-color)", "var(--text-color)", "var(--text-color)"]}/>
-    </div>
-  );
-}
 function Projects() {
-  const projects = [
+
+  const Portfolio = () => {
+    return (
+      <div className="portfolio">
+        <img src="/folio.png" alt="portfolio" width="85%"/>
+        <p>A React app showcasing my work, including a dynamic CSS grid layout and interactive designs. Built with Vite and hosted via Github Pages.</p>
+      </div>
+    );
+  };
+  
+  const Bounce = () => {
+    return (
+      <div className="bounce">
+        <video src="/bounce_demo.MOV" autoPlay loop playsInline />
+        <p>An iOS arcade game built with Swift and published to the App Store featuring monetized ads, leaderboard, and achievements.</p>
+      </div>
+    );
+  }
+  
+  const Loader = () => {
+    return (
+      <div className="spinner">
+        <FidgetSpinner height={200} width={440} backgroundColor="var(--highlight-color)" ballColors={["var(--text-color)", "var(--text-color)", "var(--text-color)"]}/>
+      </div>
+    );
+  }
+
+  const projects = useMemo(() => [
     {
       title: "This Portfolio",
       project: <Portfolio />,
@@ -43,7 +45,7 @@ function Projects() {
       title: "work in progress",
       project: <Loader />,
     }
-  ];
+  ], [Portfolio, Bounce, Loader]);
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
